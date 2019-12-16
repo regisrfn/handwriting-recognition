7037a732d7b072492d94b43af634c90b54bacf37
Regis Rufino <regis.rfnrodrigues@gmail.com>
2019-12-16 20:50:34 -0200

    Setting up upload image box

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="image_box">
     <div class="box__logo">
-      <img class="image_box__logo" svg-inline src="@/img/SVG/photo.svg" alt="">
+      <img class="image_box__logo" svg-inline src="@/img/SVG/cloud-backup-up-arrow.svg" alt="">
     </div>
     <div class="image_box__header">
-      <h1>Upload your image</h1>
+      <h1>UPLOAD IMAGE</h1>
     </div>
     <form action="#" class="form">
       <div class="form-item">
