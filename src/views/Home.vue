b4da25a1732c0da801524d17d470d920b0b83cec
Regis Rufino <regis.rfnrodrigues@gmail.com>
2019-12-15 23:57:34 -0200

    Creating upload component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="home">
-    test
+  <div class="image_box">
+    <div class="image_box__header">
+      <h1>Upload your image</h1>
+    </div>
+    <form action="#" class="form">
+      <div class="form-item">
+        <label for="">Image</label>
+        <input type="file" id="image">
+      </div>
+    </form>
   </div>
 </template>
