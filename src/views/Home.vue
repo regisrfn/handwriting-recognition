cca3f110058aed57d2ad6133bb45424c7ca23b5c
Regis Rufino <regis.rfnrodrigues@gmail.com>
2019-12-17 20:38:21 -0200

    Change fonts and add button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="image_box">
     <div class="box__logo">
-      <img class="image_box__logo" svg-inline src="@/img/SVG/cloud-backup-up-arrow.svg" alt="">
+      <img class="image_box__logo" svg-inline src="@/img/SVG/cloud-backup-up-arrow.svg" alt />
     </div>
     <div class="image_box__header">
       <h1>UPLOAD IMAGE</h1>
     </div>
     <form action="#" class="form">
-      <div class="form-item">
-        <label for="">Image</label>
-        <input type="file" id="image">
+      <div class="upload-btn-wrapper">
+        <button class="btn">Choose a file</button>
+        <input type="file" name="myfile" />
       </div>
     </form>
   </div>
