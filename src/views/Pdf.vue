4db402d88dea9886848ee5f2bd2a9fc61bb86e4b
Regis Rufino <regis.rfnrodrigues@gmail.com>
2020-01-18 00:40:43 -0200

    Setting up pdf vue component

--- src/views/Pdf.vue
+++ src/views/Pdf.vue
@@ -1,7 +1,16 @@
 <template>
-    <div class="pdfForm">
-        <form action="">
-            <div class="pdfForm__box"></div>
-        </form>
-    </div>
+  <div class="pdfForm">
+    <form class="pdfForm__box">
+      <div class="pdfForm__box--logo">
+        <img svg-inline class="icon" src="../img/SVG/upload.svg" alt="example" />
+      </div>
+      <div class="pdfForm__box--heading">Table extract</div>
+      <div class="pdfForm__box--field">
+        <input type="file" class="file__input"/>
+        <button class="file__buttom" type="button">CHOOSE A FILE</button>
+        <span class="file__text">No file chosen yet</span>
+      </div>
+      <button class="pdfForm__button" type="submit">Extract</button>
+    </form>
+  </div>
 </template>
