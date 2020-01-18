14c803b42b25c9d5359c34a025fd50deaef3692b
Regis Rufino <regis.rfnrodrigues@gmail.com>
2020-01-18 13:34:58 -0200

    Refactoring layout

--- src/views/Pdf.vue
+++ src/views/Pdf.vue
@@ -6,31 +6,31 @@
       </div>
       <div class="pdfForm__box--heading">Table extract</div>
       <div class="pdfForm__box--field">
-        <input type="file" class="file__input" ref="file__input" @change="onFileChanged" />
-        <button class="file__buttom" type="button" @click="chooseFile">CHOOSE A FILE</button>
-        <span class="file__text">{{msg}}</span>
+        <input type="file" ref="file__input" @change="onFileChanged">
+        <button class="btn" type="button" @click="chooseFile">CHOOSE A FILE</button>
+        <span>{{msg}}</span>
       </div>
-      <button class="pdfForm__button" type="submit">Extract</button>
+      <button class="btn" type="submit">Extract</button>
     </form>
   </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            selectedFile:null,
-            msg: 'No file chosen yet'
-        }
+  data() {
+    return {
+      selectedFile: null,
+      msg: "No file chosen yet"
+    };
+  },
+  methods: {
+    chooseFile() {
+      this.$refs.file__input.click();
     },
-    methods:{
-        chooseFile(){
-            this.$refs.file__input.click()
-        },
-        onFileChanged(event){
-            this.selectedFile = event.target.files[0]
-            this.msg = this.selectedFile.name 
-        }
+    onFileChanged(event) {
+      this.selectedFile = event.target.files[0];
+      this.msg = this.selectedFile.name;
     }
-}
+  }
+};
 </script>
